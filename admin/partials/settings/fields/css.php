9401df450409471413438aa64941b045d0bb150d
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-08-12 15:58:26 +0300

    Update css.php

--- admin/partials/settings/fields/css.php
+++ admin/partials/settings/fields/css.php
@@ -25,34 +25,41 @@ jQuery(function() {
 
 <p class="description"><?php echo esc_html( $data['description'] ); ?></p>
 
-<h4 class="title"><?php esc_html_e( 'Available variables', 'shortcodes-ultimate' ); ?></h4>
-<table class="widefat striped" style="width:auto">
-	<thead>
-		<tr>
-			<td><?php esc_html_e( 'Variable', 'shortcodes-ultimate' ); ?></td>
-			<td><?php esc_html_e( 'Will be replaced with', 'shortcodes-ultimate' ); ?>&hellip;</td>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td><code contenteditable>%home_url%</code></td>
-			<td><?php printf( '%s (%s)', __( 'the URL of the site home page', 'shortcodes-ultimate' ), __( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
-		</tr>
-		<tr>
-			<td><code contenteditable>%theme_url%</code></td>
-			<td><?php printf( '%s (%s)', __( 'the URL of the directory of the current theme', 'shortcodes-ultimate' ), __( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
-		</tr>
-		<tr>
-			<td><code contenteditable>%plugin_url%</code></td>
-			<td><?php printf( '%s (%s)', __( 'the URL of the directory of the plugin', 'shortcodes-ultimate' ), __( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
-		</tr>
-	</tbody>
-</table>
+<details class="su-admin-settings-details">
+	<summary class="title"><?php esc_html_e( 'Available variables', 'shortcodes-ultimate' ); ?></summary>
+	<article>
+		<table class="widefat striped" style="width:auto">
+			<thead>
+				<tr>
+					<td><?php esc_html_e( 'Variable', 'shortcodes-ultimate' ); ?></td>
+					<td><?php esc_html_e( 'Will be replaced with', 'shortcodes-ultimate' ); ?>&hellip;</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><code contenteditable>%home_url%</code></td>
+					<td><?php printf( '%s (%s)', esc_html__( 'the URL of the site home page', 'shortcodes-ultimate' ), esc_html__( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
+				</tr>
+				<tr>
+					<td><code contenteditable>%theme_url%</code></td>
+					<td><?php printf( '%s (%s)', esc_html__( 'the URL of the directory of the current theme', 'shortcodes-ultimate' ), esc_html__( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
+				</tr>
+				<tr>
+					<td><code contenteditable>%plugin_url%</code></td>
+					<td><?php printf( '%s (%s)', esc_html__( 'the URL of the directory of the plugin', 'shortcodes-ultimate' ), esc_html__( 'with trailing slash', 'shortcodes-ultimate' ) ); ?></td>
+				</tr>
+			</tbody>
+		</table>
+	</article>
+</details>
 
-<h4 class="title"><?php _e( 'More information', 'shortcodes-ultimate' ); ?></h4>
-
-<ul class="ul-disc">
-	<?php // Translators: %s - link to the shortcodes.css file ?>
-	<li><?php printf( __( 'Open %s file to see default styles', 'shortcodes-ultimate' ), '<a href="' . $this->plugin_url . 'includes/css/shortcodes.css" target="_blank">shortcodes.css</a>' ); ?></li>
-	<li><?php esc_html_e( 'Help article', 'shortcodes-ultimate' ); ?>: <a href="https://getshortcodes.com/docs/how-to-use-custom-css-editor/" target="_blank"><?php esc_html_e( 'How to use Custom CSS editor', 'shortcodes-ultimate' ); ?></a></li>
-</ul>
+<details class="su-admin-settings-details">
+	<summary><?php esc_html_e( 'More information', 'shortcodes-ultimate' ); ?></summary>
+	<article>
+		<ul class="ul-disc">
+			<?php // Translators: %s - link to the shortcodes.css file ?>
+			<li><?php printf( __( 'Open %s file to see default styles', 'shortcodes-ultimate' ), '<a href="' . esc_url_raw( $this->plugin_url ) . 'includes/css/shortcodes.full.css" target="_blank">shortcodes.full.css</a>' ); ?></li>
+			<li><?php esc_html_e( 'Help article', 'shortcodes-ultimate' ); ?>: <a href="https://getshortcodes.com/docs/how-to-use-custom-css-editor/" target="_blank"><?php esc_html_e( 'How to use Custom CSS editor', 'shortcodes-ultimate' ); ?></a></li>
+		</ul>
+	</article>
+</details>
