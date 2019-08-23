04fe39d2978de1d9ad51585eb23617405aaec570
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-08-23 16:34:47 +0300

    Update settings.php

--- admin/partials/pages/settings.php
+++ admin/partials/pages/settings.php
@@ -1,4 +1,4 @@
-<?php defined( 'ABSPATH' ) or exit; ?>
+<?php defined( 'ABSPATH' ) || exit; ?>
 
 <div id="su_admin_settings" class="wrap su-admin-settings wp-clearfix">
 
@@ -8,10 +8,28 @@
 
 	<form action="options.php" method="post">
 
-		<?php settings_fields( rtrim( $this->plugin_prefix, '-_' ) ); ?>
-		<?php do_settings_sections( $this->plugin_prefix . 'settings' ); ?>
+		<?php if ( isset( $_GET['advanced'] ) ) : ?>
+
+			<div class="notice notice-warning">
+				<p><strong><?php esc_html_e( 'Warning!', 'shortcodes-ultimate' ); ?></strong> <?php esc_html_e( 'You are editing advanced settings. Be careful and do not edit settings you do not understand', 'shortcodes-ultimate' ); ?>. <a href="<?php echo esc_attr( $this->get_component_url() ); ?>"><?php esc_html_e( 'Return to general settings', 'shortcodes-ultimate' ); ?></a>.</p>
+			</div>
+
+			<?php settings_fields( $this->plugin_prefix . 'advanced-settings' ); ?>
+			<?php do_settings_sections( $this->plugin_prefix . 'advanced-settings' ); ?>
+
+		<?php else : ?>
+
+			<?php settings_fields( $this->plugin_prefix . 'settings' ); ?>
+			<?php do_settings_sections( $this->plugin_prefix . 'settings' ); ?>
+
+		<?php endif; ?>
+
 		<?php submit_button(); ?>
 
 	</form>
+
+	<?php if ( ! isset( $_GET['advanced'] ) ) : ?>
+		<p><a href="<?php echo esc_attr( add_query_arg( 'advanced', '', $this->get_component_url() ) ); ?>"><?php esc_html_e( 'Advanced settings', 'shortcodes-ultimate' ); ?></a></p>
+	<?php endif; ?>
 
 </div>
