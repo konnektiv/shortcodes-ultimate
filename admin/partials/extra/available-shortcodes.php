fdf870491f1cb0ecc684f501f051ea9c1ad968c8
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-22 16:24:21 +0300

    Update available-shortcodes.php

--- admin/partials/extra/available-shortcodes.php
+++ admin/partials/extra/available-shortcodes.php
@@ -2,7 +2,7 @@
 
 <div class="su-admin-shortcodes-extra">
 	<p class="su-admin-shortcodes-extra-message"><?php esc_html_e( 'This shortcode is available with the Extra Shortcodes add-on', 'shortcodes-ultimate' ); ?></p>
-	<img src="<?php echo esc_attr( $this->get_image_url( 'generator/icon-banner.png' ) ); ?>" class="su-admin-shortcodes-extra-icon">
+	<img src="<?php echo esc_attr( $this->get_image_url( 'icon-banner.png' ) ); ?>" class="su-admin-shortcodes-extra-icon">
 	<h2 class="su-admin-shortcodes-extra-title"><?php esc_html_e( 'Extra Shortcodes', 'shortcodes-ultimate' ); ?></h2>
 	<p class="su-admin-shortcodes-extra-description"><?php esc_html_e( 'This add-on extends Shortcodes Ultimate with 15 new shortcodes. Parallax sections, responsive content slider, pricing tables and more', 'shortcodes-ultimate' ); ?></p>
 	<p class="su-admin-shortcodes-extra-action">
